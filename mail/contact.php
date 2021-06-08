f2510478b0a325ce3d32546b5afda5a955e5e63e
Rishi Kumar <85473852+RishiKumarRajvansh@users.noreply.github.com>
2021-06-08 06:57:41 +0530

    Update contact.php

--- mail/contact.php
+++ mail/contact.php
@@ -9,7 +9,7 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "rishikumarrajvansh@gmail.com"; 
+$to = "85473852+RishiKumarRajvansh@users.noreply.github.com"; 
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
